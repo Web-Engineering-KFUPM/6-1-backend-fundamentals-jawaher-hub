b25972111c306347c2086edc5638995521d182bf
jwaher-x <jawaher.5alidfnn@gmail.com>
2026-03-30 13:31:22 +0300

    todo 1+2

--- 6-1-back-end-fundamentals/backend/server.js
+++ 6-1-back-end-fundamentals/backend/server.js
@@ -1,9 +1,10 @@
 // TODO 1: Import Express
 
-
+import express from 'express';
 
 // TODO 2: Create the Express app and store it in a variable named app
 
+const app = express();
 
 
 // TODO 3: Allow React to access the server
